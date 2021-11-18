25525528909cf872847ce11f5397ea5e361f719a
GrantPennington <gtpenn16@gmail.com>
2021-11-18 11:54:36 -0500

    11/18/21 - Grant Pennington - added basic setup for the main kcc class

--- compiler/kcc.java
+++ compiler/kcc.java
@@ -1,5 +1,42 @@
 package compiler;
 
+import java.io.IOException;
+//ANTLR packages
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.gui.Trees;
+import lexparse.*; //classes for lexer parser
+
 public class kcc{
-    
+    public static void main(String[] args){
+        CharStream input;
+        KnightCodeLexer lexer;
+        CommonTokenStream tokens;
+        KnightCodeParser parser;
+
+        try{
+            input = CharStreams.fromFileName(args[0]);  //get the input
+            lexer = new KnightCodeLexer(input); //create the lexer
+            tokens = new CommonTokenStream(lexer); //create the token stream
+            parser = new KnightCodeParser(tokens); //create the parser
+       
+            ParseTree tree = parser.file();  //set the start location of the parser
+            
+            
+            //Trees.inspect(tree, parser);
+            
+            String classFile = args[1];		
+	
+	        myListener listener = new myListener(classFile);
+	        ParseTreeWalker walker = new ParseTreeWalker();
+	        walker.walk(listener, tree);
+        
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
