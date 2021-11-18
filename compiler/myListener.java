ec495c67926abc27a808bf31fddf1f1368dc5d2e
GrantPennington <gtpenn16@gmail.com>
2021-11-18 13:39:55 -0500

    11/18/21 - Grant Pennington - started adding method extensions from the base listener, and a hashmap - 3 hours

--- compiler/myListener.java
+++ compiler/myListener.java
@@ -13,12 +13,14 @@ public class myListener extends KnightCodeBaseListener{
 	private MethodVisitor mainVisitor; //ASM MethodVisitor
 	private String programName; //name of the .class output file
 
-    //HashMap<String, Variable> varTable = new HashMap<String, Variable>(); //hash map of type String and class Variable to store the declared variables
+    HashMap<String, Variable> varTable = new HashMap<String, Variable>(); //hash map of type String and class Variable to store the declared variables
 
-    
+
 	public myListener(String programName){
+
 		this.programName = programName;
-	}//end constructor
+	
+    }//end constructor
 
 	public void setupClass(){
 		
@@ -56,4 +58,124 @@ public class myListener extends KnightCodeBaseListener{
         System.out.println("Done!");
 
 	}//end closeClass
-}
+
+    public void enterFile(KnightCodeParser.FileContext ctx){
+
+		System.out.println("Enter program rule for first time");
+		setupClass();
+	}
+
+	public void exitFile(KnightCodeParser.FileContext ctx){
+
+		System.out.println("Leaving program rule. . .");
+		closeClass();
+
+	}
+
+	public void enterDeclare(KnightCodeParser.DeclareContext ctx){
+
+		System.out.println("Enter Declare rule for first time");
+	}
+
+	public void exitDeclare(KnightCodeParser.DeclareContext ctx){
+
+		System.out.println("Leaving Declare rule. . .");
+        // for loop to view the contents of the HashMap (for testing purposes)
+        for(String key : varTable.keySet()){ 
+            System.out.println("KEY: "+key+" VALUE: "+varTable.get(key).toString()); 
+        }
+
+	}
+
+    public void enterBody(KnightCodeParser.BodyContext ctx){
+
+		System.out.println("Enter body for first time");
+	
+	}
+
+	public void exitBody(KnightCodeParser.BodyContext ctx){
+
+		System.out.println("Leaving body. . .");
+        // for loop to view the contents of the HashMap (for testing purposes)
+        for(String key : varTable.keySet()){ 
+            System.out.println("KEY: "+key+" VALUE: "+varTable.get(key).toString()); 
+        }
+
+	}
+
+    public void enterAddition(KnightCodeParser.AdditionContext ctx){
+        System.out.println("ENTER ADDITION");
+        System.out.println(ctx.getText());
+        /*
+            add ASM code to do addition
+        */
+    }
+
+    public void exitAddition(KnightCodeParser.AdditionContext ctx){}
+
+    public void enterVariable(KnightCodeParser.VariableContext ctx) { 
+        System.out.println("Enter variable rule");
+
+        String varType = ""; // string variable to hold varType
+        String identifier = ""; // string to hold the identifier
+        for(int i = 0; i < ctx.getText().length(); i++){
+            // for loop that iterates over the ctx.getText()
+            // this loop pulls out the datatype and identifier and assigns them
+            if(!Character.isLowerCase(ctx.getText().charAt(i))){
+                varType += ctx.getText().charAt(i);
+            }
+            else {
+                identifier = ""+ctx.getText().charAt(i);
+            }
+        }
+        varTable.put(identifier, new Variable(varType)); // store the values gotten from the for loop into the hash map
+        // print statements
+        System.out.println("ID: "+identifier+" TYPE: "+varType);
+        System.out.println(varTable.get(identifier).toString());
+    }
+	
+	public void exitVariable(KnightCodeParser.VariableContext ctx) { 
+        System.out.println("Exit variable rule. . .");
+    }
+
+    public void enterSetvar(KnightCodeParser.SetvarContext ctx){
+        System.out.println("Enter set var rule");
+        
+        String key = ctx.getText().substring(ctx.getText().indexOf('T')+1, ctx.getText().indexOf(':')); //substring to extract key value from SET statement
+        String value = ctx.getText().substring(ctx.getText().indexOf('=')+1); // substring to extract value from SET statement
+        
+        // update the value of the Variable object using the setter method
+        // cast the value to the proper type i.e. Integer or String
+        if(varTable.get(key).datatype == "INTEGER"){
+            varTable.get(key).setValue(Integer.parseInt(value));
+        } else {
+            varTable.get(key).setValue(value);
+        }
+    }
+
+    public void exitSetvar(KnightCodeParser.SetvarContext ctx){
+        System.out.println("Exit set var rule. . .");
+    }
+
+    public void enterStat(KnightCodeParser.StatContext ctx){
+        System.out.println("Enter stat rule");
+        System.out.println(ctx.getText());
+    }
+
+    public void exitStat(KnightCodeParser.StatContext ctx){
+        System.out.println("exit stat rule");
+    }
+
+    public void enterPrint(KnightCodeParser.PrintContext ctx){
+        System.out.println("Print: "+ctx.getText());
+    }
+
+	/**
+	 * Prints context string. Used for debugging purposes
+	 * @param ctx
+	 */
+	private void printContext(String ctx){
+		System.out.println(ctx);
+	}
+
+} //end class
