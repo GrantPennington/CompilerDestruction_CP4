f277e55209d5a595721bb7189fc1b42c66303ea0
GrantPennington <gtpenn16@gmail.com>
2021-11-18 11:55:54 -0500

    11/18/21 - Grant Pennington - added basic setup for the myListener class

---
+++ compiler/myListener.java
@@ -0,0 +1,59 @@
+package compiler;
+
+import java.util.HashMap;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import lexparse.*;
+
+public class myListener extends KnightCodeBaseListener{
+    private ClassWriter cw;  //ASM ClassWriter 
+	private MethodVisitor mainVisitor; //ASM MethodVisitor
+	private String programName; //name of the .class output file
+
+    //HashMap<String, Variable> varTable = new HashMap<String, Variable>(); //hash map of type String and class Variable to store the declared variables
+
+    
+	public myListener(String programName){
+		this.programName = programName;
+	}//end constructor
+
+	public void setupClass(){
+		
+		//Set up the classwriter
+		cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        cw.visit(Opcodes.V11, Opcodes.ACC_PUBLIC,this.programName, null, "java/lang/Object",null);
+	
+		//Use local MethodVisitor to create the constructor for the object
+		MethodVisitor mv=cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
+        mv.visitCode();
+        mv.visitVarInsn(Opcodes.ALOAD, 0); //load the first local variable: this
+        mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V",false);
+        mv.visitInsn(Opcodes.RETURN);
+        mv.visitMaxs(1,1);
+        mv.visitEnd();
+       	
+		//Use global MethodVisitor to write bytecode according to entries in the parsetree	
+	 	mainVisitor = cw.visitMethod(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC,  "main", "([Ljava/lang/String;)V", null, null);
+        mainVisitor.visitCode();
+
+	}//end setupClass
+
+	public void closeClass(){
+		//Use global MethodVisitor to finish writing the bytecode and write the binary file.
+		mainVisitor.visitInsn(Opcodes.RETURN);
+		mainVisitor.visitMaxs(3, 3);
+		mainVisitor.visitEnd();
+
+		cw.visitEnd();
+
+        byte[] b = cw.toByteArray();
+
+        Utilities.writeFile(b,this.programName+".class");
+        
+        System.out.println("Done!");
+
+	}//end closeClass
+}
